6b73075aadf9ecd8794029ec1cab788a8271fe8c
U-UNIV\emolkose <eero.molkoselka@oulu.fi>
2020-12-17 10:06:37 +0200

    index DateTime

--- server/server.cpp
+++ server/server.cpp
@@ -2,8 +2,8 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 6
-#define VERSION_PATCH 0
-#define VERSION_DEV 0
+#define VERSION_PATCH 1
+#define VERSION_DEV 1
 
 static const VersionInfo info = VersionInfo(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_DEV);
 
