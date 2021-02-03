f115e45c6fdbe927bf1eda00e611589784f517ee
U-UNIV\emolkose <eero.molkoselka@oulu.fi>
2021-02-03 12:29:23 +0200

    release 1.7.0

--- server/server.cpp
+++ server/server.cpp
@@ -3,7 +3,7 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 7
 #define VERSION_PATCH 0
-#define VERSION_DEV 1
+#define VERSION_DEV 0
 
 static const VersionInfo info = VersionInfo(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_DEV);
 
