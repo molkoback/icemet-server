1809f691762e08ae5ca27e63ca2cf1603adbaee1
molko <molkoback@gmail.com>
2023-12-21 21:11:53 +0200

    update CMakeLists.txt

--- server/server.cpp
+++ server/server.cpp
@@ -1,11 +1,17 @@
 #include "server/server.hpp"
 
+#include <string>
+
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 15
 #define VERSION_PATCH 0
 #define VERSION_DEV 1
 
-static const VersionInfo info = VersionInfo(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_DEV, ICEMETSERVER_COMMIT);
+#ifndef ICEMET_SERVER_COMMIT
+#define ICEMET_SERVER_COMMIT std::string()
+#endif
+
+static const VersionInfo info = VersionInfo(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_DEV, ICEMET_SERVER_COMMIT);
 
 const VersionInfo& icemetServerVersion()
 {
